5c570c828ba1de6450240e79696b2deb32532d12
diogodh <diogodh@gmail.com>
2020-04-28 23:32:11 +0100

    Initial commit.

---
+++ move0.py
@@ -0,0 +1,14 @@
+array = ["a",0,0,"b",None,"c","d",0,1,False,0,1,0,3,[],0,1,9,0,0,{},0,0,9]
+
+arrayd = []
+i = 0
+size = len(array) - 1
+
+while i <= len(array) - 1:
+    if array[i] == 0 and array[i] is not False:
+        arrayd.append(array.pop(i))
+        i = i - 1
+    i += 1
+
+array = array + arrayd
+print(array)
